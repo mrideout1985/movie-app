09f936a45e3b33247809426373e3084986f72eb4
Matthew <mrideout1985@gmail.com>
2021-09-05 02:06:54 +0100

    general commit

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -6,38 +6,35 @@ import { SvgMovie } from "../icons/Movie"
 import styles from "../../styles/Navbar.module.scss"
 
 const Navbar = () => {
-
 	const router = useRouter();
-
-	console.log("router", router.pathname)
-
-
 	return (
 		<nav className={styles.container} aria-label="RMDB Menu">
-			<div className={styles.nav} role="menubar">
-				<div className={styles.logo} role="none">
-					<Link href="/" role="menuitem" tabindex="-1">
-						<a>
-							<SvgMovie color={"fff"} height={50} width={50} />
-							<h1>RMDB</h1>
-						</a>
-					</Link>
-				</div>
+			<div className={styles.logo}>
+				<Link href="/" role="menuitem" tabindex="-1">
+					<a>
+						<SvgMovie color={"fff"} height={50} width={50} />
+						<h1>RMDB</h1>
+					</a>
+				</Link>
+			</div>
 
-				<div className={styles.links}>
-					<Link href="/"><a className={[
-						[styles["default"]],
-						[router.pathname === "/" ? styles["active"] : ""],
-					].join(" ")}>Movies</a></Link>
-					<Link href="/tvshows"><a className={[
-						[styles["default"]],
-						[router.pathname === "/tvshows" ? styles["active"] : ""],
-					].join(" ")}>TV</a></Link>
-					<Link href="/actors"><a className={[
-						[styles["default"]],
-						[router.pathname === "/actors" ? styles["active"] : ""],
-					].join(" ")}>Actors</a></Link>
-				</div>
+			<div className={styles.links}>
+				<Link href="/"><a className={[
+					[styles["default"]],
+					[router.pathname === "/" ? styles["active"] : ""],
+				].join(" ")}>Movies</a></Link>
+				<Link href="/tvshows"><a className={[
+					[styles["default"]],
+					[router.pathname === "/tvshows" ? styles["active"] : ""],
+				].join(" ")}>TV Shows</a></Link>
+				<Link href="/actors"><a className={[
+					[styles["default"]],
+					[router.pathname === "/actors" ? styles["active"] : ""],
+				].join(" ")}>Actors</a></Link>
+			</div>
+
+			<div className={styles.search}>
+				<input className={styles.styledinput} />
 			</div>
 		</nav>
 	)
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
+import "../styles/main.scss"
+
 
 function MyApp({ Component, pageProps }) {
 
