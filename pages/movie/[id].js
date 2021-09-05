e9945fe17541bc84f265ab4e44bc33ded24b917b
Matthew <mrideout1985@gmail.com>
2021-09-05 01:15:39 +0100

    removed test code

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -25,32 +25,7 @@ const MovieDetails = (props) => {
 
 	return (
 		<div className={styles.moviecard}>
-			<div className={styles.info} >
-				<div className={styles.header} >
-					<img className={styles.poster} src={
-						movie.poster_path !== null
-							? `https://image.tmdb.org/t/p/original${movie?.poster_path}`
-							: `https://place-hold.it/300x500/aaa/WHITE&text=NO-IMAGE-AVAILABLE&fontsize=20`
-					} alt="movie-poster" />
-					<h1>{movie?.title}</h1>
-					<h4>{movie?.release_date}</h4>
-					<span className={styles.minutes} >{movie.runtime} mins</span >
-					<p className={styles.genre} >{movie.genres.map(el => el.name).join(", ")}</p >
-				</div >
-				<div className={styles.overview} >
-					<p className={styles.text} >
-						{movie.overview}
-					</p >
-				</div >
-				<div className={styles.socialmedia} >
-					<ul>
-						<li><i className={styles.socialmedia}>share</i></li>
-						<li><i className={styles.socialmedia}></i></li >
-						<li><i className={styles.socialmedia}>chat_bubble</i></li >
-					</ul >
-				</div >
-			</div >
-			<div className={styles.blurbackground}></div >
+
 		</div >
 	)
 }
