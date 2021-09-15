efb369f10286d2c1d0b5cca14fa06b584d572fe5
Matthew <mrideout1985@gmail.com>
2021-09-15 01:08:24 +0100

    tried to address css module stylesheet not loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 import "../styles/main.scss"
-
-
+import "../.next/static/empty.css"
 function MyApp({ Component, pageProps }) {
 
   return (
