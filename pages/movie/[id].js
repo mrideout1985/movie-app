08b2cb43b06a40105bae95cf7607be404e6f7593
Matthew <mrideout1985@gmail.com>
2021-09-21 21:55:25 +0100

    styled tv and film page

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,8 +1,8 @@
 import api from "../api/index"
 import Image from "next/image"
-import Circle from "../../components/icons/Circle"
 import styles from "../../styles/MediaPage.module.scss"
 import { useMedia } from 'react-use';
+
 
 
 export const getStaticProps = async ({ params }) => {
@@ -29,11 +29,6 @@ const MovieDetails = (props) => {
 	const large = useMedia('(max-width: 1800px)');
 
 
-
-
-
-
-
 	const movie = props.movie
 
 	const handleTrailer = (movie) => {
@@ -43,40 +38,51 @@ const MovieDetails = (props) => {
 
 	if (movie) {
 		return (
-			<section className={styles.container} style={{
-				backgroundImage: `
+
+			<div className={styles.container}>
+				<div className={styles.card} style={{
+					backgroundImage: `
 			url(https://image.tmdb.org/t/p/original${movie?.backdrop_path})`
-			}}>
-
-				<div className={styles.card}>
-					<Image className={styles.image}
-						src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} layout="fixed" alt="image" width={phone ? 240 : 0
-							|| tab_port ? 300 : 0
-								|| tab_land ? 350 : 0
-									|| large ? 500 : 0} height={phone ? 380 : 0
-										|| tab_port ? 480 : 0
-											|| tab_land ? 520 : 0
-												|| large ? 700 : 0} />
-
-					<div className={styles.information}>
-						<div><h2>{movie.title}</h2>
-							{movie.tagline ? <h3>&#34;{movie.tagline}&#34;</h3> : null}
-							<div className={styles.additionalinfo}>
-								{movie.vote_average ? <Circle rating={movie.vote_average} height={"2rem"} color="#80ED99" /> : null}&nbsp;&nbsp;{movie.release_date ? <div className={styles.release_date}>{movie.release_date.substring(0, 4)}</div> : null}&nbsp;&nbsp;|&nbsp;&nbsp;<div className={styles.genres}>{movie.genres.map(genre => genre.name).join(", ")}</div>
+				}}>
+					<div className={styles.mediaInfo}   >
+						<div className={styles.image}>
+							<Image
+								src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} layout="fixed" alt="image" width={phone ? 240 : 0
+									|| tab_port ? 300 : 0
+										|| tab_land ? 350 : 0
+											|| large ? 450 : 450} height={phone ? 380 : 0
+												|| tab_port ? 480 : 0
+													|| tab_land ? 520 : 0
+														|| large ? 650 : 650} />
+						</div>
+						<div className={styles.generalInfo}>
+							<div className={styles.title}>
+								<h1>{movie.title}</h1>
+								<div className={styles.tagline}>{movie.tagline}</div>
+								<div className={styles.rating}>
+									{movie.vote_average ? <h3>{movie.vote_average}</h3> : null}&nbsp;&nbsp;-&nbsp;&nbsp;{movie.release_date ? <div className={styles.release_date}>{movie.release_date.substring(0, 4)}</div> : null}&nbsp;&nbsp;-&nbsp;&nbsp;<div className={styles.genres}>{movie.genres.map(genre => genre.name).join(", ")}</div>
+								</div>
+								<div className={styles.overview}>
+									{movie.overview}
+								</div>
 							</div>
-							<p>{movie.overview}</p>
+							<div className={styles.trailer}>
+								<iframe
+									id="ytplayer"
+									src={`https://www.youtube.com/embed/${handleTrailer(movie)}?autoplay=0`}
+									frameBorder="0"
+
+								></iframe>
+							</div>
 						</div>
-						<div className={styles.trailer}>
-							<iframe
-								id="ytplayer"
-								src={`https://www.youtube.com/embed/${handleTrailer(movie)}?autoplay=0`}
-								frameBorder="0"
-							></iframe>
-						</div>
+
+					</div>
+					<div className={styles.cast}>
 					</div>
 				</div>
+			</div>
 
-			</section >
+
 		)
 	} else {
 		return null
--- pages/tv/[id].js
+++ pages/tv/[id].js
@@ -1,6 +1,6 @@
 import api from "../api/index"
 import Image from "next/image"
-import Circle from "../../components/icons/Circle"
+import { useMedia } from 'react-use';
 import styles from "../../styles/MediaPage.module.scss"
 
 export const getStaticProps = async ({ params }) => {
@@ -23,6 +23,10 @@ const TvDetails = (props) => {
 
 	const tv = props.tv
 
+	const phone = useMedia('(max-width: 600px)');
+	const tab_port = useMedia('(max-width: 815px)');
+	const tab_land = useMedia('(max-width: 1140px)');
+	const large = useMedia('(max-width: 1800px)');
 
 
 	const handleTrailer = (tv) => {
@@ -32,38 +36,49 @@ const TvDetails = (props) => {
 
 	if (tv) {
 		return (
-			<section className={styles.container} style={{
-				backgroundImage: `
+
+			<div className={styles.container}>
+				<div className={styles.card} style={{
+					backgroundImage: `
 			url(https://image.tmdb.org/t/p/original${tv?.backdrop_path})`
-			}}>
-
-				<div className={styles.card}>
-					<div className={styles.imagecontainer}>
-						<Image className={styles.image} src={`https://image.tmdb.org/t/p/original${tv?.poster_path}`} alt="image" width={450} height={700} />
-					</div>
-
-					<div className={styles.information}>
-						<div><h2>{tv.name}</h2>
-							{tv.tagline ? <h3>&#34;{tv.tagline}&#34;</h3> : null}
-							<div className={styles.additionalinfo}>
-								{tv.vote_average ? <Circle rating={tv.vote_average} height={"2rem"} color="#80ED99" /> : null}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{tv.first_air_date ? tv.first_air_date.substring(0, 4) : null}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{tv.genres.map(genre => genre.name).join(", ")}
+				}}>
+					<div className={styles.mediaInfo}   >
+						<div className={styles.image}>
+							<Image
+								src={`https://image.tmdb.org/t/p/original${tv?.poster_path}`} layout="fixed" alt="image" width={phone ? 240 : 0
+									|| tab_port ? 300 : 0
+										|| tab_land ? 350 : 0
+											|| large ? 450 : 450} height={phone ? 380 : 0
+												|| tab_port ? 480 : 0
+													|| tab_land ? 520 : 0
+														|| large ? 650 : 650} />
+						</div>
+						<div className={styles.generalInfo}>
+							<div className={styles.title}>
+								<h1>{tv.name}</h1>
+								<div className={styles.tagline}>{tv.tagline}</div>
+								<div className={styles.rating}>
+									{tv.vote_average ? <h3>{tv.vote_average}</h3> : null}&nbsp;&nbsp;-&nbsp;&nbsp;{tv.release_date ? <div className={styles.release_date}>{tv.release_date.substring(0, 4)}</div> : null}&nbsp;&nbsp;-&nbsp;&nbsp;<div className={styles.genres}>{tv.genres.map(genre => genre.name).join(", ")}</div>
+								</div>
+								<div className={styles.overview}>
+									{tv.overview}
+								</div>
 							</div>
-							<p>{tv.overview}</p>
+							<div className={styles.trailer}>
+								<iframe
+									id="ytplayer"
+									src={`https://www.youtube.com/embed/${handleTrailer(tv)}?autoplay=0`}
+									frameBorder="0"
+
+								></iframe>
+							</div>
 						</div>
-						<div className={styles.trailer}>
-							<iframe
-								id="ytplayer"
-								width="640"
-								height="360"
-								src={`https://www.youtube.com/embed/${handleTrailer(tv)}?autoplay=0`}
-								frameBorder="0"
-							></iframe>
-						</div>
+
+					</div>
+					<div className={styles.cast}>
 					</div>
 				</div>
-
-			</section >
-		)
+			</div>)
 	} else {
 		return null
 	}
